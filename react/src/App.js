0dc9f685e42a1c4acaf7b070979d03f0688e5698
skaysrei <alerocco90@gmail.com>
2022-07-01 14:58:02 +0200

    update

--- react/src/App.js
+++ react/src/App.js
@@ -2,15 +2,37 @@ import './App.css';
 import Grid from './components/grid.jsx';
 import Keyboard from './components/keyboard.jsx';
 import RandWord from './components/randWord';
+import {React, useState, useEffect} from 'react';
 
 
 function App() {
+
+  const axios = require('axios').default;
+  const [output, setOutput] = useState();
+
+  useEffect(()=> {
+    axios.get(
+      'https://random-word-api.herokuapp.com/word',
+      {
+        params:
+          { 
+            length : 5,
+            lang : 'it'
+          }
+      }
+    ).then(
+      resp => { setOutput(resp.data)}
+    );
+    console.log("Render Here")
+  }, [])
+
   return (
     <div>
       <header className="App-header">
         <div>
           Random Word:
-          <RandWord />
+          {output}
+          {/*<RandWord />*/}
         </div>
         <Grid />
         <Keyboard />
