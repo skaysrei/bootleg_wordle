ce21858e6556b1456268909e541a0c569613a340
skaysrei <alerocco90@gmail.com>
2022-07-01 11:52:48 +0200

    Base Project

---
+++ react/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Grid from './components/grid.jsx';
+import Keyboard from './components/keyboard.jsx';
+
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <Grid />
+        <Keyboard />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/src/components/keyboard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Keyboard() {
+  return (
+    <div>K</div>
+  )
+}
