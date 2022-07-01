70644b230f1e506744290348d1a8cb041bc9a04c
skaysrei <alerocco90@gmail.com>
2022-07-01 12:01:14 +0200

    .

---
+++ react/src/components/randWord.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Axios from 'axios'
+
+export default function RandWord() {
+  const axios = require('axios').default;
+
+  Axios() 
+  
+  return (
+    <div>
+      {}
+    </div>
+  )
+}
